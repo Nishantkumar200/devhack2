20c381e4e2a970308bc48cbab68c114d19cae333
Nishant kumar <nishantkumarsharma0@gmail.com>
2020-02-15 09:31:22 +0530

    second register

--- register.php
+++ register.php
@@ -6,7 +6,7 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/style1.css">
 <title>Registeration</title>
 </head>
 <body>
@@ -21,21 +21,97 @@
                     <form action="">
                         <div class="form-group">
                             <label for="name">Name:</label>
+                            <input type="text" class="form-control" name="" id="" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="mobile">Mobile:</label>
                             <input type="text" class="form-control" name="" id="">
                         </div>
                         <div class="form-group">
-                            <label for="mobile"><i class="fas fa-mobile"></i>Mobile:</label>
+                            <label for="email">Email</label>
                             <input type="text" class="form-control" name="" id="">
                         </div>
                         <div class="form-group">
-                            <label for="email"> <i class="fas fa-envelope-square"></i>Email</label>
+                            <label for="Address">Address</label>
+                            <textarea name="" class="form-control" id="" cols="15" rows="5"></textarea>
+                        </div>
+                        <div class="form-group">
+                           <label for="crop">Crop Preference</label>
+                          <input type="text" class="form-control" name="" id="">
+                          <br>
+                          <input type="text" class="form-control" name="" id="">
+                          <br>
+                          <input type="text" class="form-control" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <label for="Username">Username</label>
                             <input type="text" class="form-control" name="" id="">
                         </div>
+                        <div class="form-group">
+                            <label for="Password">Password</label>
+                            <input type="password" class="form-control" name="" id="">
+                        </div>
+                       <!-- Default unchecked -->
+                       <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="defaultUnchecked">
+                         <label class="custom-control-label" for="defaultUnchecked">I have read the <a href="#">terms and condition.</a></label>
+                        </div>
+                        <button type="submit" class="btn btn-block btn-primary mt-2">Register</button>
+                        <small><i><u>Already have an account</u></i><a href="index.php"> Login</a></small>
                     </form>
                 </div>
             </div>
         </div>
         
+<!-- for company -->
+<div class="col-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="text-center text-white">Company</h3>
+                </div>
+                <div class="card-body">
+                    <form action="">
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" class="form-control" name="" id="" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="mobile">Mobile:</label>
+                            <input type="text" class="form-control" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <label for="Address">Address</label>
+                            <textarea name="" class="form-control" id="" cols="15" rows="5"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="industrytype">Industry Type</label>
+                            <input type="text" class="form-control" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <label for="Username">Username</label>
+                            <input type="text" class="form-control" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <label for="Password">Password</label>
+                            <input type="password" class="form-control" name="" id="">
+                        </div>
+                       <!-- Default unchecked -->
+                       <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="defaultUnchecked">
+                         <label class="custom-control-label" for="defaultUnchecked">I have read the <a href="#">terms and condition</a></label>
+                        </div>
+                        <button type="submit" class="btn btn-block btn-primary mt-2">Register</button>
+                        <small><i><u>Already have an account</u></i><a href="index.php"> Login</a></small>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
